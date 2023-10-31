7329f39c86c49e1913f3cd766fedbfe21a0e5ea2
margaretmaher711 <margaretmaher11@gmail.com>
2023-10-31 22:29:09 +0200

    commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -13,55 +13,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // void _incrementCounter() async {
-  //   DatabaseHelper dbHelper = DatabaseHelper();
-  //
-  //   // Initialize the database
-  //   await dbHelper.initDB();
-  //
-  //   // Example usage of database operations
-  //   List<Map<String, dynamic>> items = await dbHelper.getItems();
-  //   print('Items: $items');
-  //
-  //   Map<String, dynamic> newItem = {
-  //     DatabaseHelper.itemId: 'item12',
-  //     DatabaseHelper.itemName: 'Example Item',
-  //     DatabaseHelper.itemBarcode: '123456789',
-  //     DatabaseHelper.itemPrice: 9.99,
-  //     DatabaseHelper.itemQuantity: 10,
-  //   };
-  //
-  //   try {
-  //     int insertedItemId = await dbHelper.insertItem(newItem);
-  //     print('Item inserted. Item ID: $insertedItemId');
-  //   } catch (e) {
-  //     if (e is DatabaseException) {
-  //       if (e.isUniqueConstraintError()) {
-  //         print('Item insertion failed: Duplicate barcode $e');
-  //       } else {
-  //         print('Item insertion failed: ${e.result}');
-  //       }
-  //     } else {
-  //       print('Item insertion failed: $e');
-  //     }
-  //   }
-  //
-  //   int lastDocumentNumber = await dbHelper.getLastDocumentNumber();
-  //   DateTime currentDateTime = await dbHelper.getCurrentDateTime();
-  //
-  //   Map<String, dynamic> newStockRecord = {
-  //     DatabaseHelper.recordDocNo: lastDocumentNumber,
-  //     DatabaseHelper.recordTime: currentDateTime.toString(),
-  //     DatabaseHelper.recordItemId: 'item123',
-  //     DatabaseHelper.recordItemQuantity: 5,
-  //   };
-  //   print('newStockRecord: $newStockRecord');
-  //   int insertedStockRecordId =
-  //   await dbHelper.insertStockRecord(newStockRecord);
-  //   print('Stock record inserted. Record ID: $insertedStockRecordId');
-  //   var ktkt = await dbHelper.getLastDocumentNumber();
-  //   print('ktkt${ktkt}');
-  // }
+  void _incrementCounter() async {
+    DatabaseHelper dbHelper = DatabaseHelper();
+
+    // Initialize the database
+    await dbHelper.initDB();
+
+    // Example usage of database operations
+    List<Map<String, dynamic>> items = await dbHelper.getItems();
+    print('Items: $items');
+
+    Map<String, dynamic> newItem = {
+      DatabaseHelper.itemId: 'item12',
+      DatabaseHelper.itemName: 'Example Item',
+      DatabaseHelper.itemBarcode: '123456789',
+      DatabaseHelper.itemPrice: 9.99,
+      DatabaseHelper.itemQuantity: 10,
+    };
+
+    try {
+      int insertedItemId = await dbHelper.insertItem(newItem);
+      print('Item inserted. Item ID: $insertedItemId');
+    } catch (e) {
+      if (e is DatabaseException) {
+        if (e.isUniqueConstraintError()) {
+          print('Item insertion failed: Duplicate barcode $e');
+        } else {
+          print('Item insertion failed: ${e.result}');
+        }
+      } else {
+        print('Item insertion failed: $e');
+      }
+    }
+
+    int lastDocumentNumber = await dbHelper.getLastDocumentNumber();
+    DateTime currentDateTime = await dbHelper.getCurrentDateTime();
+
+    Map<String, dynamic> newStockRecord = {
+      DatabaseHelper.recordDocNo: lastDocumentNumber,
+      DatabaseHelper.recordTime: currentDateTime.toString(),
+      DatabaseHelper.recordItemId: 'item123',
+      DatabaseHelper.recordItemQuantity: 5,
+    };
+    print('newStockRecord: $newStockRecord');
+    int insertedStockRecordId =
+    await dbHelper.insertStockRecord(newStockRecord);
+    print('Stock record inserted. Record ID: $insertedStockRecordId');
+    var ktkt = await dbHelper.getLastDocumentNumber();
+    print('ktkt${ktkt}');
+  }
 
 
   @override
@@ -95,7 +95,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          _incrementCounter();
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
--- lib/view/new_document_screen.dart
+++ lib/view/new_document_screen.dart
@@ -18,8 +18,27 @@ class _NewDocumentScreenState extends State<NewDocumentScreen> {
 
   TextEditingController quantityController = TextEditingController();
 
-  List documentItems = [];
-  List documentItemsMap = [];
+  List<Item> documentItems = []; // Maintain a list of items
+
+  void addItem(
+    String barcode,
+    int quantity,
+    String name,
+  ) {
+    bool foundMatch = false;
+
+    for (var item in documentItems) {
+      if (item.barcode == barcode) {
+        item.quantity += quantity;
+        foundMatch = true;
+        break;
+      }
+    }
+
+    if (!foundMatch) {
+      documentItems.add(Item(barcode, quantity, name));
+    }
+  }
 
   void showSnackBar(BuildContext context, message, backGroundColor) {
     final snackBar = SnackBar(
@@ -38,7 +57,7 @@ class _NewDocumentScreenState extends State<NewDocumentScreen> {
 
     List<Map<String, dynamic>> result = await db!.query(
       'items',
-      columns: ['item_name', 'item_price', 'item_quantity','item_barcode'],
+      columns: ['item_name', 'item_price', 'item_quantity', 'item_barcode'],
       where: 'item_barcode = ?',
       whereArgs: [barcode],
       limit: 1,
@@ -52,23 +71,27 @@ class _NewDocumentScreenState extends State<NewDocumentScreen> {
       //
       // }
 
-      documentItemsMap[documentItemsMap.indexWhere((element) {
-        return element['documentItemBarcode'] == itemData['item_barcode'];
-      })] =
-          itemData;
-
-      documentItemsMap.add({
-        "documentItemName": itemData['item_name'],
-        "documentItemQuantity": itemData['item_quantity'],
-        "documentItemBarcode": itemData['item_barcode'],
-      });
-      documentItems.add(ItemInDocument(
-          itemName: itemData['item_name'],
-          itemQuantity: itemData['item_quantity']));
+      // documentItemsMap[documentItemsMap.indexWhere((element) {
+      //   return element['documentItemBarcode'] == itemData['item_barcode'];
+      // })] =
+      //     itemData;
+      addItem(
+        itemData['item_barcode'],
+        int.parse(quantityController.text),
+        itemData['item_name'],
+      );
+      // documentItemsMap.add({
+      //   "documentItemName": itemData['item_name'],
+      //   "documentItemQuantity": itemData['item_quantity'],
+      //   "documentItemBarcode": itemData['item_barcode'],
+      // });
+      // documentItems.add(ItemInDocument(
+      //     itemName: itemData['item_name'],
+      //     itemQuantity: itemData['item_quantity']));
       barcodeController.text = '';
       quantityController.text = '1';
-      print('result.first${documentItemsMap}');
       print('result.first${documentItems}');
+      // print('result.first${documentItems}');
       print('result.first${itemData['item_barcode']}');
     } else {
       itemData = {};
@@ -96,15 +119,18 @@ class _NewDocumentScreenState extends State<NewDocumentScreen> {
     print('Stock record inserted. Record ID: $insertedStockRecordId');
   }
 
-  Future<int> updateItemQuantity(String itemBarcode, int newQuantity) async {
+  Future<int> updateItemQuantity(List<Item> itemsInDocument) async {
     Database? db = await dbHelper.database;
+    int rowsAffected=0;
+    itemsInDocument.forEach((element) async {
+       rowsAffected = await db!.update(
+        'items',
+        {'item_quantity': element.quantity},
+        where: 'item_barcode = ?',
+        whereArgs: [element.barcode],
+      );
+    });
 
-    int rowsAffected = await db!.update(
-      'items',
-      {'item_quantity': newQuantity},
-      where: 'item_barcode = ?',
-      whereArgs: [itemBarcode],
-    );
     print('rowsAffected$rowsAffected');
     return rowsAffected;
   }
@@ -112,111 +138,126 @@ class _NewDocumentScreenState extends State<NewDocumentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'Document No. 1',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Row(
-            children: [
-              Column(
-                children: [
-                  const Text(
-                    'Barcode',
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: 260,
-                    child: CustomTextField(
-                      labelTxt: '',
-                      hintTxt: '',
-                      textController: barcodeController,
-                      validator: (String? str) {},
-                      onFieldSubmitted: (String? str) {},
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  const Text(
-                    'Qty.',
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: 100,
-                    child: CustomTextField(
-                      labelTxt: '',
-                      hintTxt: '',
-                      textController: quantityController,
-                      validator: (String? str) {},
-                      onFieldSubmitted: (String? str) {},
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                backgroundColor: Theme.of(context).colorScheme.primary,
-              ),
-              onPressed: () {
-                getItemByBarcode(barcodeController.text);
-              },
-              child: const Text('Add')),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              itemBuilder: ((context, index) {
-                // return myMap[index] as Widget;
-                return documentItems[index];
-              }),
-              itemCount: documentItems.length,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 330,
-          ),
-          documentItems.isNotEmpty
-              ? Align(
-                  alignment: AlignmentDirectional.bottomCenter,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor:
-                            Theme.of(context).colorScheme.onPrimary,
-                        backgroundColor: Theme.of(context).colorScheme.primary,
+            const Text(
+              'Document No. 1',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      'Barcode',
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      width: 260,
+                      child: CustomTextField(
+                        labelTxt: '',
+                        hintTxt: '',
+                        textController: barcodeController,
+                        validator: (String? str) {},
+                        onFieldSubmitted: (String? str) {},
                       ),
-                      onPressed: () {
-                        insertToRecordsTable();
-                        updateItemQuantity(
-                                '123456789', int.parse(quantityController.text))
-                            .then((value) {
-                          showSnackBar(context, 'Document Saved Successfully.',
-                              Colors.green);
-                          Get.back();
-                        });
-                      },
-                      child: const Text('Save')),
-                )
-              : Container(),
-        ],
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      'Qty.',
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: CustomTextField(
+                        labelTxt: '',
+                        hintTxt: '',
+                        textController: quantityController,
+                        validator: (String? str) {},
+                        onFieldSubmitted: (String? str) {},
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                ),
+                onPressed: () {
+                  print('barcodeController.text${barcodeController.text}');
+                  getItemByBarcode(barcodeController.text);
+                },
+                child: const Text('Add')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemBuilder: ((context, index) {
+                  // return myMap[index] as Widget;
+                  return ItemInDocument(
+                      itemName: documentItems[index].name,
+                      itemQuantity: documentItems[index].quantity);
+                }),
+                itemCount: documentItems.length,
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 50,
+        margin: const EdgeInsets.all(10),
+        child: documentItems.isNotEmpty
+            ? Align(
+                alignment: AlignmentDirectional.bottomCenter,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                    ),
+                    onPressed: () {
+                      // print(
+                      //     'quantityController.text${quantityController.text}');
+                      insertToRecordsTable();
+                      updateItemQuantity(
+                              documentItems)
+                          .then((value) {
+                        showSnackBar(context, 'Document Saved Successfully.',
+                            Colors.green);
+                        Get.back();
+                      });
+                    },
+                    child: const Text('Save')),
+              )
+            : Container(),
+      ),
+    );
   }
+}
+
+class Item {
+  final String barcode;
+  String name;
+  int quantity;
+
+  Item(this.barcode, this.quantity, this.name);
 }
